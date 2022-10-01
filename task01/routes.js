81b07a16e34d9653784ade6162c0e4bf7f2f3318
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-01 16:48:00 -0300

    organize code

--- task01/routes.js
+++ task01/routes.js
@@ -3,24 +3,60 @@ const routesHandler = (req,res) => {
   // console.log('URL:', req.url, 'METHOD:', req.method, 'HEADERS:', req.headers)
 
   const url = req.url
-  const body = '<html><head><title>Task01</title></head><body></body></html>'
-
-  res.setHeader('Content-Type', 'text/html')
-
-  res.write(body)
+  const method = req.method
 
   if (url === '/') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head>')
+    res.write('<title>Task 01</title>')
+    res.write('<body>')
     res.write('<h1>Home</h1>')
     res.write('<p>Hello! This is home.</p>')
-    res.write('<form action="/create-user" method="POST"><label for="add">Add a user: </label><input type="text" name="user"><button type="submit">Add</button></form>')
+    res.write('<form action="/create-user" method="POST">')
+    res.write('<label for="add">Add a user: </label>')
+    res.write('<input type="text" name="user">')
+    res.write('<button type="submit">Add</button>')
+    res.write('</form>')
+    res.write('</body>')
+    res.write('</head>')
+    res.write('</html>')
+    
+    return res.end()
   }
 
   if (url === '/users') {
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head>')
+    res.write('<title>Task 01</title>')
+    res.write('<body>')
     res.write('<h1>Users</h1>')
     res.write('<ul><li>User 01</li></ul>')
+    res.write('</body>')
+    res.write('</head>')
+    res.write('</html>')
+
+    return res.end()
   }
 
-  res.end()
+  if (url === '/create-user' && method === 'POST') {
+    const body = []
+    
+    req.on('data', (chunk) => {
+      body.push(chunk)
+    })
+    
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString()
+      console.log('Parsed Body: ', parsedBody)
+    })
+    
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    
+    return res.end()
+  }
 }
 
 module.exports = routesHandler
