e40a7fe28dab30c39103119532661219b07d3d43
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-01 15:50:07 -0300

    routes "/" and "/users"

--- task01/routes.js
+++ task01/routes.js
@@ -1,9 +1,21 @@
 const routesHandler = (req,res) => {
   console.log('server is running...')
   // console.log('URL:', req.url, 'METHOD:', req.method, 'HEADERS:', req.headers)
+
+  const url = req.url
+  const html1 = '<html><head><title>Task01</title></head><body>'
+  const html2 = '</body></html>'
   
   res.setHeader('Content-Type', 'text/html')
-  res.write('<h1>this is a test<h1/>')
+
+  if (url === '/') {
+    res.write(`${html1}<h1>Home</h1><p>Hello! This is home.</p>${html2}`)
+  }
+
+  if (url === '/users') {
+    res.write(`${html1}<h1>Users</h1><ul><li>User 01</li></ul>${html2}`)
+  }
+
   res.end()
 }
 
