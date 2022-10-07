c0d2e29d522c720daef85826e6f2a528dedef597
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-07 16:18:07 -0300

    split 404 page into another file

--- task03/app.js
+++ task03/app.js
@@ -4,12 +4,10 @@ const app = express()
 
 const homeRoute = require('./routes/home')
 const usersRoute = require('./routes/users')
+const notFoundRoute = require('./routes/404')
 
 app.use(usersRoute)
 app.use(homeRoute)
-
-app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>')
-})
+app.use(notFoundRoute)
 
 app.listen(3000)
