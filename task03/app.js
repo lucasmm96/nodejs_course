d2b5b4420db3abba531467a77832ba8f200804c3
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-07 15:44:47 -0300

    add users route

--- task03/app.js
+++ task03/app.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.use('/users', (req, res, next) => {
+  console.log('middleware /users');
+  res.send('<h1>this is users</h1>')
+  next()
+})
+
 app.use('/', (req, res, next) => {
   console.log('middleware /');
   res.send('<h1>this is home</h1>')
