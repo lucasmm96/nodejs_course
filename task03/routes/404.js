0a87d8580bbf0d853ee978bba6ae44dcd6403ec7
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-07 16:31:57 -0300

    set html file response for 404

--- task03/routes/404.js
+++ task03/routes/404.js
@@ -1,10 +1,12 @@
+const path = require('path')
+
 const express = require('express')
 
 const router = express.Router()
 
 router.use((req, res, next) => {
   console.log('middleware /404')
-  res.status(404).send('<h1>Page not found</h1>')
+  res.sendFile(path.join(__dirname, '..', 'views', '404.html'))
 })
 
 module.exports = router
