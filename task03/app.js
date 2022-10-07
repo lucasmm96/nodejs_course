013be438572b316754d570365edc694e35c00ab8
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-07 16:13:59 -0300

    404 page

--- task03/app.js
+++ task03/app.js
@@ -6,7 +6,10 @@ const homeRoute = require('./routes/home')
 const usersRoute = require('./routes/users')
 
 app.use(usersRoute)
-
 app.use(homeRoute)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000)
