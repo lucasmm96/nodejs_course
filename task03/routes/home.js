43f7a93f598187e82cd46b41719fc30d8c21bc3e
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-07 16:13:47 -0300

    change from 'user' to 'get'

--- task03/routes/home.js
+++ task03/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-router.use('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   console.log('middleware /');
   res.send('<h1>this is home</h1>')
 })
