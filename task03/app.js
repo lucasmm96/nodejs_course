4463e2c2dc2d9bf155340d5067d6a57b92fd6807
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-07 16:01:24 -0300

    split users route into another file

--- task03/app.js
+++ task03/app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 
 const homeRoute = require('./routes/home')
+const usersRoute = require('./routes/users')
+
+app.use(usersRoute)
 
 app.use(homeRoute)
 
---
+++ task03/routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.use('/users', (req, res, next) => {
+  console.log('middleware /users');
+  res.send('<h1>this is users</h1>')
+})
+
+module.exports = router
