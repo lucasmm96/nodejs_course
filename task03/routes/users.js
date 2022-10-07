2a1212f730b447a5536800baceb4c3ce80facab6
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-07 16:29:53 -0300

    set html file response for users

--- task03/routes/users.js
+++ task03/routes/users.js
@@ -1,10 +1,12 @@
+const path = require('path')
+
 const express = require('express')
 
 const router = express.Router()
 
 router.use('/users', (req, res, next) => {
   console.log('middleware /users')
-  res.send('<h1>this is users</h1>')
+  res.sendFile(path.join(__dirname, '..', 'views', 'users.html'))
 })
 
 module.exports = router
