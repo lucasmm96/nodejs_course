99379c4855ad01d09878f31490ee2efd18d389f6
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-07 17:56:23 -0300

    create and link external style file

--- task03/app.js
+++ task03/app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express')
 
 const app = express()
@@ -5,6 +7,8 @@ const app = express()
 const homeRoute = require('./routes/home')
 const usersRoute = require('./routes/users')
 const notFoundRoute = require('./routes/404')
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(usersRoute)
 app.use(homeRoute)
