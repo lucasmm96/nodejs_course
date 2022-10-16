d97b149c7c439823209576b67bd4844675635547
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-16 16:23:23 -0300

    home route

---
+++ task04/routes/home.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const users = []
+
+router.get('/', (req, res) => {
+  res.status(200).render('home', { pageTitle: 'Home' })
+})
+
+router.post('/add-user', (req, res) => {
+  users.push(req.body.name)
+  res.redirect('/')
+})
+
+exports.router = router
+exports.users = users
