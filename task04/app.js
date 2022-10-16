c381fbd6aa97b98c244cad0f3245b850c29225d9
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-16 16:23:50 -0300

    link routes

--- task04/app.js
+++ task04/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+const homeRoute = require('./routes/home')
+const usersRoute = require('./routes/users')
 
 app.set('view engine', 'pug')
 app.set('views', 'views')
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/users', (req, res) => {
-  res.status(200).render('users', { pageTitle: 'Users' })
+  res.status(200).render('users', { pageTitle: 'Users' , usersList: homeRoute.users })
 })
-app.get('/', (req, res) => {
-  res.status(200).render('home', { pageTitle: 'Home' })
+
+app.use(homeRoute.router)
+app.use(usersRoute)
+app.use((req, res) => {
+  res.render('404')
 })
 
 app.listen(3000)
