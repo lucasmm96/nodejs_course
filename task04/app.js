b3addbccb04a35f122c4435697aa5409ede4c155
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-16 15:35:07 -0300

    add users route

--- task04/app.js
+++ task04/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.set('view engine', 'pug')
 app.set('views', 'views')
 
+app.get('/users', (req, res) => {
+  res.status(200).render('users', { pageTitle: 'Users' })
+})
 app.get('/', (req, res) => {
   res.status(200).render('home', { pageTitle: 'Home' })
 })
