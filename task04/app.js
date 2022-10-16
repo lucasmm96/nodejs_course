d0c3d639677f9596dc0599b2bbca484da28a1dfc
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2022-10-16 16:33:47 -0300

    adjust syntax

--- task04/app.js
+++ task04/app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
-const homeRoute = require('./routes/home')
-const usersRoute = require('./routes/users')
+const bodyParser = require('body-parser');
+const homeRoute = require('./routes/home');
+const usersRoute = require('./routes/users');
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/users', (req, res) => {
-  res.status(200).render('users', { pageTitle: 'Users' , usersList: homeRoute.users })
+  res.status(200).render('users', { pageTitle: 'Users' , usersList: homeRoute.users });
 })
 
-app.use(homeRoute.router)
-app.use(usersRoute)
+app.use(homeRoute.router);
+app.use(usersRoute);
 app.use((req, res) => {
-  res.render('404')
+  res.render('404');
 })
 
-app.listen(3000)
+app.listen(3000);
--- task04/routes/users.js
+++ task04/routes/users.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const home = require('./home')
+const express = require('express');
+const router = express.Router();
+const home = require('./home');
 
 router.get('/users', (req, res) => {
-  res.status(200).render('users', { pageTitle: 'Users', users: home.users})
+  res.status(200).render('users', { pageTitle: 'Users', users: home.users});
 })
 
-module.exports = router
+module.exports = router;
